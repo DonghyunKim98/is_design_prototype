112ca0546812abf38bf35424086c16d5e987b601
DongHyunKim <akainoo123@gmail.com>
2022-09-23 21:58:38 +0900

    feat: title 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
 
 import { LocaleProvider, UIProvider } from '@/provider';
@@ -15,6 +16,12 @@ function MyApp({ Component, pageProps }: AppProps) {
     <RecoilRoot>
       <UIProvider>
         <LocaleProvider>
+          <Head>
+            <title>통합 챗봇</title>
+            <link rel="icon" href="/favicon.ico" />
+            <meta property="og:title" content="통합 챗봇 - 모든 민원을 하나의 사이트에서!" />
+            <meta name="description" content="이제 모든 민원을 한 곳에서 관리해보세요!" />
+          </Head>
           <Component {...pageProps} />
         </LocaleProvider>
       </UIProvider>
