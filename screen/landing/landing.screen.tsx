3843fe5679bf214b8d14c52d4bd2d9f6d6c8a648
DongHyunKim <akainoo123@gmail.com>
2022-09-26 11:40:27 +0900

    fix: mobile

--- screen/landing/landing.screen.tsx
+++ screen/landing/landing.screen.tsx
@@ -28,11 +28,13 @@ export const LandingScreen: NextPage = () => {
 
   return (
     <FormProvider {...methods}>
-      <Box className="flex items-center justify-center w-screen h-screen gap-4">
-        <Box className="flex flex-col flex-none w-full h-full bg-gray-100 md:w-3/4 md:h-5/6 md:aspect-5/9 lg:w-96 lg:h-auto lg:aspect-5/9">
-          <LandingChatHeaderModule />
-          <LandingChatBubblesModule />
-          <LandingTextFieldModule />
+      <Box className="absolute inset-0">
+        <Box className="flex items-center justify-center w-screen h-screen gap-4">
+          <Box className="flex flex-col flex-none w-screen h-screen bg-gray-100 md:w-3/4 md:h-5/6 md:aspect-5/9 lg:w-96 lg:h-auto lg:aspect-5/9">
+            <LandingChatHeaderModule />
+            <LandingChatBubblesModule />
+            <LandingTextFieldModule />
+          </Box>
         </Box>
       </Box>
       <LandingAddBotResponseModule />
